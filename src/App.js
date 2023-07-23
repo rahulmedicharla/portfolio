268ebbacb2914f5a5e8adc48a64226d70170911e
rahulmedicharla <rmedicharla@gmail.com>
2023-07-22 22:03:43 -0400

    scroll icons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Container, Row, Col} from 'react-bootstrap';
 import Navigation from './components/Nav/Navigation';
 import Header from './components/Header/Header';
 import Landing from './components/Landing/Landing';
+import ScrollDown from './components/ScrollDown/ScrollDown';
 
 function App() {
   return (
@@ -14,11 +15,17 @@ function App() {
         </Col>
       </Row>
       <Row>
-        <Col xs={10} md={10} className='d-flex'>
+        <Col xs={10} md={8} lg={6} className='d-flex align-items-center justify-content-center'>
           <Landing name={content.name} description={content.description} role={content.role} aboutTag = {content.anchorTags[1]}/>
         </Col>
-        <Col xs={2} md={2} className='d-flex justify-content-center'>
+        <Col xs={2} md={4} lg={4}></Col>
+        <Col xs={0} md={0} lg={2}className='d-flex justify-content-center'>
           <Navigation anchorTags = {content.anchorTags} links = {content.links}/>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={12} lg={12} className='d-flex justify-content-center'>
+          <ScrollDown/>
         </Col>
       </Row>
     </Container>
